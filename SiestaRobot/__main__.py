2adcc099463de5852d45f82f464768cc1049415a
Blank <93943699+Blank-sama@users.noreply.github.com>
2022-02-23 17:46:14 +0530

    Update __main__.py

--- SiestaRobot/__main__.py
+++ SiestaRobot/__main__.py
@@ -82,7 +82,7 @@ def get_readable_time(seconds: int) -> str:
 
     return ping_time
 
-
+#hlo rukka
 SIESTA_IMG = "https://telegra.ph/file/6d3e3ef24f64fb6e35df0.jpg"
 
 DONATE_STRING = """Heya, glad to hear you want to donate!
